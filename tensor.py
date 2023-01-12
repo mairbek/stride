a350abdaa986c71035abe9c90db5042e2bff5245
Mairbek Khadikov <mkhadikov@gmail.com>
2023-01-11 20:14:40 -0800

    wip: normalize idx

--- tensor.py
+++ tensor.py
@@ -1,22 +1,50 @@
 class Storage:
     def __init__(self, n: int) -> None:
         self.arr = [0] * n
+
+
+accessor = slice | int
+
+
 class Tensor:
-    # TODO also offset
-    def __init__(self, storage: Storage, sizes: tuple[int, int], strides: tuple[int, int]) -> None:
+    def __init__(self, storage: Storage, sizes: tuple[int, int], strides: tuple[int, int], offset: int = 0) -> None:
         self.storage = storage
         self.sizes = sizes
         self.strides = strides
+        self.offset = offset
 
-    def __getitem__(self, idx: int | tuple[int, int] | slice) -> int:
+    def normalize_index(self, idx: accessor | tuple[accessor] | tuple[accessor, accessor]) -> tuple[slice, slice]:
         if isinstance(idx, int):
-            # TODO check dims
-            if self.sizes[1] != 1:
-                raise ValueError("Not a 1D tensor")
-            return self.storage.arr[idx]
-        if isinstance(idx, tuple) and len(idx) == 2:
-            index = idx[0] * self.strides[0] + idx[1] * self.strides[1]
-            return self.storage.arr[index]
+            idx = [idx,]
+        if isinstance(idx, tuple):
+            idx = list(idx)
+        if len(idx) == 1:
+            idx.append(slice(0, self.sizes[1]))
+        if len(idx) != 2:
+            raise ValueError("Invalid index")
+        for i in range(2):
+            if isinstance(idx[i], int):
+                idx[i] = slice(idx[i], idx[i] + 1)
+        return tuple(idx)
+
+    def __getitem__(self, idx: accessor | tuple[accessor] | tuple[accessor, accessor]) -> int:
+        norm_idx = self.normalize_index(idx)
+#        if isinstance(idx, int):
+#            # TODO check dims
+#            if self.sizes[1] != 1:
+#                raise ValueError("Not a 1D tensor")
+#            return self.storage.arr[idx]
+#        if isinstance(idx, tuple) and len(idx) == 2:
+#            index = idx[0] * self.strides[0] + idx[1] * self.strides[1]
+#            return self.storage.arr[index]
+#        if isinstance(idx, slice):
+#            # TODO check dims
+#            if self.sizes[1] != 1:
+#                raise ValueError("Not a 1D tensor")
+#            start = idx.start if idx.start is not None else 0
+#            stop = idx.stop if idx.stop is not None else self.sizes[0]
+#            step = idx.step if idx.step is not None else 1
+#            return [self.storage.arr[i] for i in range(start, stop, step)]
 
         raise NotImplementedError
 
--- tensor_test.py
+++ tensor_test.py
@@ -1,6 +1,18 @@
 import pytest
 import stride
 
+
+def test_normalize_index():
+    t = stride.zeros(5)
+    assert t.normalize_index(0) == (slice(0, 1), slice(0, 1))
+
+    t = stride.zeros(5, 4)
+    assert t.normalize_index(1) == (slice(1, 2), slice(0, 4))
+    assert t.normalize_index((1, 2)) == (slice(1, 2), slice(2, 3))
+    assert t.normalize_index((1, slice(0, 3))) == (slice(1, 2), slice(0, 3))
+    assert t.normalize_index((slice(0, 3), 2)) == (slice(0, 3), slice(2, 3))
+
+
 def test_onedim_getandsetitem():
     t = stride.zeros(5)
 
@@ -10,6 +22,7 @@ def test_onedim_getandsetitem():
 
     for i in range(5):
         assert t[i] == i * 2
+
 
 def test_twodim_getandsetitem():
     t = stride.zeros(5, 4)
@@ -22,3 +35,20 @@ def test_twodim_getandsetitem():
     for i in range(5):
         for j in range(4):
             assert t[i, j] == i + j + 2
+
+
+def test_onedim_range():
+    t = stride.zeros(5)
+    for i in range(5):
+        t[i] = i * 2
+
+    assert t[1:3] == [2, 4]
+
+
+def test_twodim_range():
+    t = stride.zeros(5, 4)
+    for i in range(5):
+        for j in range(4):
+            t[i, j] = i + j + 2
+
+    assert t[1, 1:3] == [2, 4]
