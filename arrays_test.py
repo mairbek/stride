95ccbbb3089ae5f0d2d4e00d5df6b8f7cb84cbba
Mairbek Khadikov <mkhadikov@gmail.com>
2023-03-15 22:29:50 -0700

    stash

--- arrays_test.py
+++ arrays_test.py
@@ -1,6 +1,16 @@
 import pytest
 import stride
 
+
+def test_zeros():
+    a = stride.zero_array((3, ))
+    assert a.shape == (3,)
+    assert a.payload == [0, 0, 0]
+
+    a = stride.zero_array((3, 4))
+    assert a.shape == (3, 4)
+    assert a.payload == [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
 def test_onedim_arrays():
     a = stride.array([1, 2, 3])
 
@@ -16,3 +26,14 @@ def test_multidim_arrays():
 
     a = stride.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     assert a.shape == (3, 3)
+
+def test_broadcast_failure():
+    a = stride.array([1, 2, 3])
+    b = stride.broadcast_to(a, (2, 3))
+    assert b.payload == [[1, 2, 3], [1, 2, 3]]
+    
+def test_out():
+    a = stride.array([1, 2, 3])
+    b = stride.out(a, 2)
+    print("fek")
+    assert b == 0
--- stride.py
+++ stride.py
@@ -1,10 +1,20 @@
 from tensor import Storage
 from tensor import Tensor
-from arrays import Array
+
+import arrays
 
 def zeros(n: int, m: int = 1):
     s = Storage(n * m)
     return Tensor(s, (n, m), (m, 1))
 
+def zero_array(shape):
+    return arrays.zeros(shape)
+
 def array(payload):
-    return Array(payload)
+    return arrays.Array(payload)
+
+def out(arr, ndim):
+    return arrays.out(arr, ndim)
+
+def broadcast_to(arr, shape):
+    return arrays.broacast_to(arr, shape)
